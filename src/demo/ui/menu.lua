7a18f11042835e15655d58086636ae3b5dbbb2e6
Henrik Ilgen <henrik.ilgen@activevb.de>
2016-07-30 21:33:55 +0200

    Changed label arrangement to test grid

--- src/demo/ui/menu.lua
+++ src/demo/ui/menu.lua
@@ -35,7 +35,7 @@ return {
 	content = {
 		grid,
 		rows = {
-			{ height = 100 },
+			{ height = 300 },
 			{ height = "*" },
 			{ height = "auto" }
 		},
@@ -45,17 +45,17 @@ return {
 			{ width = "auto" }
 		},
 		children = {
-			{ label, ["grid.row"] = 1, ["grid.column"] = 1, text = "1-1" },
-			{ label, ["grid.row"] = 1, ["grid.column"] = 2, text = "1-2" },
-			{ label, ["grid.row"] = 1, ["grid.column"] = 3, text = "1-3" },
+			{ label, ["grid.row"] = 1, ["grid.column"] = 1, text = "1-1", verticalAlignment = "start", horizontalAlignment = "start" },
+			{ label, ["grid.row"] = 1, ["grid.column"] = 2, text = "1-2", verticalAlignment = "start", horizontalAlignment = "start" },
+			{ label, ["grid.row"] = 1, ["grid.column"] = 3, text = "1-3", verticalAlignment = "start", horizontalAlignment = "start" },
 			
-			{ label, ["grid.row"] = 2, ["grid.column"] = 1, text = "2-1" },
-			{ label, ["grid.row"] = 2, ["grid.column"] = 2, text = "2-2" },
-			{ label, ["grid.row"] = 2, ["grid.column"] = 3, text = "2-3" },
+			{ label, ["grid.row"] = 2, ["grid.column"] = 1, text = "2-1", verticalAlignment = "center", horizontalAlignment = "center" },
+			{ label, ["grid.row"] = 2, ["grid.column"] = 2, text = "2-2", verticalAlignment = "center", horizontalAlignment = "center" },
+			{ label, ["grid.row"] = 2, ["grid.column"] = 3, text = "2-3", verticalAlignment = "center", horizontalAlignment = "center" },
 			
-			{ label, ["grid.row"] = 3, ["grid.column"] = 1, text = "3-1", verticalAlignment = "end" },
-			{ label, ["grid.row"] = 3, ["grid.column"] = 2, text = "3-2", verticalAlignment = "end" },
-			{ label, ["grid.row"] = 3, ["grid.column"] = 3, text = "3-3", verticalAlignment = "end" }
+			{ label, ["grid.row"] = 3, ["grid.column"] = 1, text = "3-1", verticalAlignment = "end", horizontalAlignment = "end" },
+			{ label, ["grid.row"] = 3, ["grid.column"] = 2, text = "3-2", verticalAlignment = "end", horizontalAlignment = "end" },
+			{ label, ["grid.row"] = 3, ["grid.column"] = 3, text = "3-3", verticalAlignment = "end", horizontalAlignment = "end" }
 		}
 	}
 }
